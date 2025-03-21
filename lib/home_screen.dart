90a42ef78caee36a68716484094f7128a4711ff9
eaponce18 <eddie.ponce@unadeca.net>
2025-03-20 18:02:15 -0600

    chat

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,194 +1,105 @@
 import 'package:flutter/material.dart';
 import 'detail_screen.dart';
+import 'search_helper.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Radiología Interactiva')),
-      body: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            // Imagen de fondo
-            Image.asset(
-              'assets/skull2.jpg',
-              fit: BoxFit.contain,
-              height: MediaQuery.of(context).size.height * 0.8,
-            ),
-            
-            // Botones transparentes sobre la imagen
-            Positioned(
-              top: 20, // Ajusta estas posiciones según tu imagen
-              child: InkWell(
-                onTap: () => _openDetailScreen(context, 'Cabeza'),
-                child: Container(
-                  width: 80,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: Colors.blue.withOpacity(0.3),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Cabeza',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            
-            Positioned(
-              top: 120, // Ajusta según necesites
-              child: InkWell(
-                onTap: () => _openDetailScreen(context, 'Torax'),
-                child: Container(
-                  width: 80,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: Colors.green.withOpacity(0.3),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Torax',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
+      appBar: AppBar(
+        title: Text('Radiología Interactiva'),
+        actions: [
+          AnimatedHelpButton(
+            onPressed: () => _mostrarAsistente(context),
+          ),
+        ],
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return Container(
+            width: constraints.maxWidth,
+            height: constraints.maxHeight,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.9,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        // Imagen de fondo
+                        Image.asset(
+                          'assets/skull2.jpg',
+                          fit: BoxFit.contain,
+                          height: constraints.maxHeight * 0.8,
+                        ),
+                        
+                        // Botones con sus posiciones originales
+                        Positioned(
+                          top: 90,
+                          child: InkWell(
+                            onTap: () => _openDetailScreen(context, 'Cabeza'),
+                            child: _buildButton('Cabeza', Colors.blue),
+                          ),
+                        ),
+                        
+                        Positioned(
+                          top: 200,
+                          child: InkWell(
+                            onTap: () => _openDetailScreen(context, 'Torax'),
+                            child: _buildButton('Torax', Colors.blue),
+                          ),
+                        ),
 
-            Positioned(
-              top: 140, // Ajusta según necesites
-              left: 90,
-              child: InkWell(
-                onTap: () => _openDetailScreen(context, 'Miembros Superiores'),
-                child: Container(
-                  width: 70,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: Colors.purple.withOpacity(0.3),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Miembros Superiores',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
+                        Positioned(
+                          top: 270,
+                          left: 30,
+                          child: InkWell(
+                            onTap: () => _openDetailScreen(context, 'Miembros Superiores'),
+                            child: _buildButton('Miembros Superiores', Colors.blue),
+                          ),
+                        ),
 
-            Positioned(
-              top: 420, // Ajusta según necesites
-              right: 115,
-              child: InkWell(
-                onTap: () => _openDetailScreen(context, 'Miembros Inferiores'),
-                child: Container(
-                  width: 80,
-                  height: 80,
-                  decoration: BoxDecoration(
-                    color: Colors.yellow.withOpacity(0.3),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Miembros Inferiores',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
+                        Positioned(
+                          top: 450,
+                          right: 80,
+                          child: InkWell(
+                            onTap: () => _openDetailScreen(context, 'Miembros Inferiores'),
+                            child: _buildButton('Miembros Inferiores', Colors.blue),
+                          ),
+                        ),
 
-            Positioned(
-              top: 90, // Ajusta según necesites
-              child: InkWell(
-                onTap: () => _openDetailScreen(context, 'Columna'),
-                child: Container(
-                  width: 70,
-                  height: 25,
-                  decoration: BoxDecoration(
-                    color: Colors.yellow.withOpacity(0.3),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Columna',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
+                        Positioned(
+                          top: 140,
+                          child: InkWell(
+                            onTap: () => _openDetailScreen(context, 'Columna'),
+                            child: _buildButton('Columna', Colors.blue),
+                          ),
+                        ),
 
-            Positioned(
-              top: 190, // Ajusta según necesites
-              child: InkWell(
-                onTap: () => _openDetailScreen(context, 'Abdomen'),
-                child: Container(
-                  width: 80,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    color: Colors.yellow.withOpacity(0.3),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Abdomen',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
+                        Positioned(
+                          top: 250,
+                          child: InkWell(
+                            onTap: () => _openDetailScreen(context, 'Abdomen'),
+                            child: _buildButton('Abdomen', Colors.blue),
+                          ),
+                        ),
+                        
+                        Positioned(
+                          top: 300,
+                          child: InkWell(
+                            onTap: () => _openDetailScreen(context, 'Pelvis'),
+                            child: _buildButton('Pelvis', Colors.blue),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ),
+                ],
               ),
             ),
-            
-            Positioned(
-              top: 235, // Ajusta según necesites
-              child: InkWell(
-                onTap: () => _openDetailScreen(context, 'Pelvis'),
-                child: Container(
-                  width: 80,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: Colors.red.withOpacity(0.3),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Pelvis',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
@@ -199,4 +110,287 @@ class HomeScreen extends StatelessWidget {
       MaterialPageRoute(builder: (context) => DetailScreen(region: region)),
     );
   }
+
+  void _mostrarAsistente(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AsistenteDialog();
+      },
+    );
+  }
+}
+
+class AnimatedHelpButton extends StatefulWidget {
+  final VoidCallback onPressed;
+
+  AnimatedHelpButton({required this.onPressed});
+
+  @override
+  _AnimatedHelpButtonState createState() => _AnimatedHelpButtonState();
+}
+
+class _AnimatedHelpButtonState extends State<AnimatedHelpButton>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _scaleAnimation = Tween<double>(
+      begin: 1.0,
+      end: 1.2,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaleTransition(
+      scale: _scaleAnimation,
+      child: Container(
+        margin: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 8,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: IconButton(
+          icon: Stack(
+            children: [
+              Icon(
+                Icons.help_outline,
+                color: Colors.white,
+                size: 30,
+              ),
+              Positioned(
+                right: 0,
+                top: 0,
+                child: Container(
+                  padding: EdgeInsets.all(2),
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.circle,
+                  ),
+                  constraints: BoxConstraints(
+                    minWidth: 8,
+                    minHeight: 8,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          onPressed: widget.onPressed,
+          tooltip: '¿Necesitas ayuda?',
+        ),
+      ),
+    );
+  }
+}
+
+class AsistenteDialog extends StatefulWidget {
+  @override
+  _AsistenteDialogState createState() => _AsistenteDialogState();
+}
+
+class _AsistenteDialogState extends State<AsistenteDialog> {
+  final TextEditingController _consultaController = TextEditingController();
+  String? _resultado;
+
+  void _buscarProyeccion(String consulta) {
+    consulta = consulta.toLowerCase();
+    final resultado = RadiologiaHelper.guiaRadiologica[consulta];
+
+    if (resultado != null) {
+      setState(() {
+        _resultado = 'Para ${consulta} se recomienda:\n'
+            'Proyección: ${resultado['proyeccion']}\n'
+            'En la región: ${resultado['region']}\n'
+            'Subregión: ${resultado['subregion']}';
+      });
+
+      // Opcional: navegar automáticamente a la proyección
+      Future.delayed(Duration(seconds: 2), () {
+        Navigator.of(context).pop();
+        _navegarAProyeccion(context, resultado);
+      });
+    } else {
+      setState(() {
+        _resultado = 'No encontré una proyección específica para esa consulta.\n'
+            'Prueba con términos más específicos o consulta el menú principal.';
+      });
+    }
+  }
+
+  void _navegarAProyeccion(BuildContext context, Map<String, String> info) {
+    // Navegar a través de las pantallas hasta llegar a la proyección específica
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => DetailScreen(region: info['region']!),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              '¿Qué necesitas encontrar?',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: _consultaController,
+              decoration: InputDecoration(
+                hintText: 'Ej: luxación de clavícula',
+                border: OutlineInputBorder(),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () => _buscarProyeccion(_consultaController.text),
+                ),
+              ),
+              onSubmitted: _buscarProyeccion,
+            ),
+            SizedBox(height: 16),
+            if (_resultado != null)
+              Container(
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Text(_resultado!),
+              ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    _mostrarSugerencias(context);
+                  },
+                  child: Text('Ver sugerencias comunes'),
+                ),
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Cerrar'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _mostrarSugerencias(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Consultas frecuentes',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 12),
+                ...RadiologiaHelper.guiaRadiologica.keys.map((consulta) =>
+                  ListTile(
+                    title: Text(consulta),
+                    onTap: () {
+                      Navigator.of(context).pop();
+                      _buscarProyeccion(consulta);
+                    },
+                  ),
+                ).toList(),
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Cerrar'),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+Widget _buildButton(String text, Color color) {
+  return Container(
+    width: 100,
+    height: 40,
+    decoration: BoxDecoration(
+      color: Colors.blue.withOpacity(0.3),
+      borderRadius: BorderRadius.circular(10),
+      border: Border.all(
+        color: Colors.blue.withOpacity(0.5),
+      ),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black26,
+          blurRadius: 4,
+          offset: Offset(0, 2),
+        ),
+      ],
+    ),
+    child: Center(
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 14,
+          shadows: [
+            Shadow(
+              offset: Offset(1, 1),
+              blurRadius: 3,
+              color: Colors.black54,
+            ),
+          ],
+        ),
+        textAlign: TextAlign.center,
+      ),
+    ),
+  );
 } 
---
+++ lib/search_helper.dart
@@ -0,0 +1,20 @@
+class RadiologiaHelper {
+  static final Map<String, Map<String, String>> guiaRadiologica = {
+    'luxación de clavícula': {
+      'proyeccion': 'Serendipity',
+      'region': 'Miembros Superiores',
+      'subregion': 'Hombro',
+    },
+    'fractura de cadera': {
+      'proyeccion': 'AP',
+      'region': 'Pelvis',
+      'subregion': 'Pelvis Ósea',
+    },
+    'sinusitis': {
+      'proyeccion': 'Waters',
+      'region': 'Cabeza',
+      'subregion': 'Senos Paranasales',
+    },
+    // Agregar más casos aquí
+  };
+} 
